a13242f2d275a9be0266526bd27cf8db8086f67f
Kapil Joshi <kapildevjoshi2@gmail.com>
2026-07-02 12:20:22 +0545

    python projects

--- project1.py
+++ project1.py
@@ -5,54 +5,28 @@ token = "56c4977935f284d0-2473cf174906088-178254ed4f7db50c"
 user_id = "lsPph8fpPvYKrEapzeQvBA=="
 viber_url = "https://chatapi.viber.com/pa/post"
 
-
-
-payload={
-     
-"auth_token":token,
-"from":user_id,
-"type":"text",
-"text":"this is test"
-
-
+payload = {
+    "auth_token":token,
+    "from":user_id,
+    "type":"text",
+    "text":"this is test"
 }
 
+payload_image = {
+    "auth_token":token,
+    "from":user_id,
+    "type":"picture",
+    "text":"this is test",
+    "media":"https://www.images.com/img.jpg",
+    
+}
 
-def send_message_viber(mmessgae=""):
-    payload['text'] = mmessgae
+def send_message_to_viber(message,pic):
+    payload_image['text']=message
+    payload_image['media']=pic
+    payload_image['thumbnail']=pic
 
-    r =requests.post(url=viber_url,data=json.dumps(payload)) 
-
+    r = requests.post(url=viber_url, data=json.dumps(payload_image))
     print(r)
-
-send_message_viber()
-
-
-url = "https://markets.onlinekhabar.com/smtm/stock_live/sector-performance"
-r = requests.get(url=url)
-
-if r.status_code == 200:
-            data = r.json()
-            response = data['response']
-
-            positive = []
-            negative = []
-
-            for item in response:
-                if item['points_change'] < 0:
-                    send_message_viber(f"{item['indices']} - ve")
-
-                    negative.append(item['indices'])
-                else:
-                    send_message_viber(f"{item['indices']} + ve")
-
-                    positive.append(item['indices'])
-
-                
-
-            print("positive------------>", len(positive))
-            print("negative------------>", len(negative))
-
-
 
 
